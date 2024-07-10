914584ebdacf8ffbf20edc9e4e7bf2a66326a7a6
R0MEN <kozinaroman7@gmail.com>
2024-07-10 18:16:31 +0300

    trashtite

---
+++ login.js
@@ -0,0 +1,38 @@
+let login
+let pass
+let logPass
+let credentials; // Оголошуємо глобальну змінну
+
+async function getLogPass() {
+    let res = await fetch("./logPass.json");
+    if (!res.ok) return "Some error";
+    let data = await res.json();
+    return data;
+}
+
+async function fetchCredentials() {
+    let logPass = await getLogPass();
+    credentials = {
+        login: decryptPassword(logPass.login),
+        pass: decryptPassword(logPass.pass)
+    };
+    console.log("Login:", credentials.login); // Вивести значення для перевірки
+    console.log("Password:", credentials.pass);
+ // Вивести значення для перевірки
+}
+
+fetchCredentials().then(() => {
+    // Використовувати змінну credentials поза асинхронною функцією
+    console.log("Credentials outside async function:", credentials.login);
+});
+
+
+
+function decryptPassword(encryptedPassword) {
+    let decryptedPassword = "";
+    let reversedEncryptedPassword = encryptedPassword.split("").reverse().join("");
+    for (let i = 0; i < reversedEncryptedPassword.length; i += 3) {
+        decryptedPassword += reversedEncryptedPassword[i + 2];
+    }
+    return decryptedPassword;
+}
